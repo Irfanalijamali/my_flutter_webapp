251121b7f789247a9bc785ec56e82f83f846761c
Irfanali0101 <irfanaliquest@gmail.com>
2025-10-22 05:47:47 -0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/deploy_shopefy_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.deploy_shopefy_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+// lib/main.dart
+import 'package:deploy_shopefy_flutter/home.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Superstar Parts',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Home(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
